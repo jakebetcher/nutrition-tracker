c2b58377fe5e671f305e747122bc66607401e483
Jake Betcher <jakebetcher@gmail.com>
2018-02-06 10:38:01 -0800

    display results to user

--- public/app.js
+++ public/app.js
@@ -1,17 +1,103 @@
 'use strict'
 
-function renderNutritionSearchPage() {
+function storeTheFood() {
+	let theFood = $('.js-food-query').val();
+	return theFood;
+}
+
+function onFoodSearchFormSubmit () {
+        $('.nutrition-search-form').val('');
+        return true; 
+    }
+
+function handleNutritionSearchClick() {
+	$('.nutrition-search-button').on('click', function(event) {
+		$('.nutrition-search-form').removeClass('hidden');
+		$('.signup-form').addClass('hidden');
+	});
+}
+
+function handleSubmitSearchFoods() {
+	$('.nutrition-search-form').submit(function(event) {
+		event.preventDefault();
+		getDataFromAPI(renderFoodResult);
+	});
+}
+
+function getDataFromAPI(callback) {
+	const food = storeTheFood();
+	const nutrientsURL = `https://api.nal.usda.gov/ndb/search/?format=json&q=${food}&sort=n&max=25&offset=0&api_key=70ONpAfW6gpIaUBzICUb8m2mRnXzs0pqI4hel3L3`;
+        const settings = {
+        url: nutrientsURL,
+        dataType: 'json',
+        type: 'GET',
+        success: callback
+        //error: displayErrorMessage()
+        };
+        $.ajax(settings);
+}
+
+
+function renderFoodResult(data) {
+	const foodListLength = data.list.item.length;
+	for (let i = 0; i < foodListLength; i++) {
+		$('.js-results-div').append(`<div class='theFoods'><span>Food Name: </span> <span class='food-name'>${data.list.item[i].name}</span></div>`);
+	}
+	
+}
+
+
+function logAPIData(data) {
+	console.log(data);
+}
+
+
+function initApp() {
+ 	handleNutritionSearchClick();
+ 	handleSubmitSearchFoods();
+}
+
+$(initApp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function renderProfilePage() {
 	return `
-		<form class='nutrition-search-form'>
-		<legend class='nutrition-search-legend'>Search for all kinds of foods and find their nutritional information.</legend>
-			<div>
-				<label for='search-nutrition'>Enter Food Name Here: </label>
-				<input name='search-nutrition' id='search-nutrition' class='search-nutrition-input' placeholder="Reese's Puffs" required>
-			</div>
-			<div>
-				<button class='food-search-button' type="submit">Search Foods</button>
-			</div>	
-		</form>
+
 	`;
 }
 
@@ -28,6 +114,14 @@ function handleNutritionSearchClick() {
 		$('body').append(renderNutritionSearchPage());
 	});
 	backToHome();
+	handleSearchFoods();
 }
 
-handleNutritionSearchClick();
+
+function handleSearchFoods() {
+	$('.nutrition-search-form').submit(function() {
+		$('body').append('<p>yay</p>');
+	});
+}
+
+//handleNutritionSearchClick();*/
