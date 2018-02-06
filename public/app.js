7ae4eb4ee35980a516d65e31ff1a3b5522021cf1
Jake Betcher <jakebetcher@gmail.com>
2018-02-06 13:30:45 -0800

    add modal

--- public/app.js
+++ public/app.js
@@ -41,20 +41,53 @@ function getDataFromAPI(callback) {
 function renderFoodResult(data) {
 	const foodListLength = data.list.item.length;
 	for (let i = 0; i < foodListLength; i++) {
-		$('.js-results-div').append(`<div class='theFoods'><span>Food Name: </span> <span class='food-name'>${data.list.item[i].name}</span></div>`);
+		$('.js-results-div').append(`<div class='theFoods'><span>Food Name: </span> <button value=${data.list.item[i].ndbno} class='food-name'>${data.list.item[i].name}</button></div>`);
 	}
 	
 }
 
-
-function logAPIData(data) {
-	console.log(data);
+function displayModal() {
+	$('.js-results-div').on('click', '.food-name', function(event) {
+		let theId = '';
+		theId = $(this).val();
+		getDetailedDataFromAPI(theId, renderDetailedData);
+	});
 }
+
+
+
+function getDetailedDataFromAPI(id, callback) {
+	const theURL = `https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=70ONpAfW6gpIaUBzICUb8m2mRnXzs0pqI4hel3L3&nutrients=208&nutrients=203&nutrients=204&nutrients=205&nutrients=291&nutrients=269&nutrients=301&nutrients=306&nutrients=401&nutrients=606&nutrients=430&nutrients=303&nutrients=307&nutrients=318&nutrients=324&nutrients=418&nutrients=415&nutrients=431&nutrients=323&ndbno=${id}`;
+	const settings = {
+		url: theURL,
+		dataType: 'json',
+		type: 'GET',
+		success: callback
+	};
+	$.ajax(settings);
+}
+
+function renderDetailedData(result) {
+	let theModal = `<div class='pop-outer'><div class='pop-inner'><button class='close'>X</button><h2>Nutritional Facts</h2>`;
+	for (let i = 0; i < result.report.foods[0].nutrients.length; i++) {
+		theModal += `<p><span>${result.report.foods[0].nutrients[i].nutrient}: </span> <span>${result.report.foods[0].nutrients[i].value} ${result.report.foods[0].nutrients[i].unit}</span></p>`
+	}
+	theModal += `<button>Add Food Item</button></div></div>`;
+	$('.js-results-div').append(theModal);
+	$('.pop-outer').fadeIn();
+	$('.js-results-div').on('click', '.close', function(event) {
+    $('.pop-outer').fadeOut();
+  });
+}
+
+
+
 
 
 function initApp() {
  	handleNutritionSearchClick();
  	handleSubmitSearchFoods();
+ 	displayModal();
 }
 
 $(initApp);
