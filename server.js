24afd68fe6d42695bcda61b4747848939aa9520d
Jake Betcher <jakebetcher@gmail.com>
2018-02-26 12:20:06 -0800

    fix PUT endpoint error

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/goals', (req, res) => {
     });
 });
 
-app.post('/goals/:id', (req, res) => {
+app.put('/goals/:id', (req, res) => {
 	if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     res.status(400).json({
       error: 'Request path id and request body id values must match'
