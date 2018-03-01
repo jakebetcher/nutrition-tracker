63d371046aba636acb8d51be8c18ea5f0ccb2a08
Jake Betcher <jakebetcher@gmail.com>
2018-03-01 10:01:53 -0800

    add POST route for creating a new user

--- users/models.js
+++ users/models.js
@@ -34,3 +34,15 @@ UserSchema.methods.serialize = function() {
     stats: this.stats
   };
 };
+
+UserSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
